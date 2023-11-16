d5b89fe28734a2dd9f21ea0315a2fea6057b4fde
Umesh Sehta <usehta@gmail.com>
2023-11-16 12:51:45 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+// service-worker.js
+
+self.addEventListener('push', function(event) {
+  const options = {
+    body: event.data.text(),
+    icon: 'path/to/icon.png', // Replace with your icon path
+    data: {
+      // You can include custom data for the notification
+      // Example: id, link, etc.
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
